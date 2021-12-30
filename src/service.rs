98e80f64af88e833b69aeb940830493bc7a3aabd
Juhana Helovuo <juhana.helovuo@atostek.com>
2021-12-30 15:55:42 +0200

    Some flesh to service skeletons.

--- src/service.rs
+++ src/service.rs
@@ -1,6 +1,9 @@
+use std::io;
+
+use mio::{Evented, Poll, Token, PollOpt, Ready,};
 
 use crate::message::Message;
-use std::marker::PhantomData;
+use crate::pubsub::{Publisher,Subscription};
 
 pub trait Service {
     type Request: Message;
@@ -9,14 +12,61 @@ pub trait Service {
 
 
 pub struct Server<S:Service> {
-    service_type: PhantomData<S>,
+  request_receiver: Subscription<S::Request>,
+  response_sender: Publisher<S::Response>,
 }
 
-// impl Evented for Server {
+impl<S:Service> Evented for Server<S> {
+  fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()> {
+    self
+      .request_receiver
+      .register(poll, token, interest, opts)
+  }
 
-// }
+  fn reregister(
+    &self,
+    poll: &Poll,
+    token: Token,
+    interest: Ready,
+    opts: PollOpt,
+  ) -> io::Result<()> {
+    self
+      .request_receiver
+      .reregister(poll, token, interest, opts)
+  }
+
+  fn deregister(&self, poll: &Poll) -> io::Result<()> {
+    self.request_receiver.deregister(poll)
+  }
+
+}
 
 pub struct Client<S:Service> {
-    service_type: PhantomData<S>,
+  request_sender: Publisher<S::Request>,
+  response_receiver: Subscription<S::Response>,
+}
+
+impl<S:Service> Evented for Client<S> {
+  fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()> {
+    self
+      .response_receiver
+      .register(poll, token, interest, opts)
+  }
+
+  fn reregister(
+    &self,
+    poll: &Poll,
+    token: Token,
+    interest: Ready,
+    opts: PollOpt,
+  ) -> io::Result<()> {
+    self
+      .response_receiver
+      .reregister(poll, token, interest, opts)
+  }
+
+  fn deregister(&self, poll: &Poll) -> io::Result<()> {
+    self.response_receiver.deregister(poll)
+  }
 
 }
