87ff2c4fe9d3759ea49824d19164a6bd6ca66780
Juhana Helovuo <juhana.helovuo@atostek.com>
2023-11-20 13:23:00 +0200

    Simplify async listener example.

--- examples/async_listener/main.rs
+++ examples/async_listener/main.rs
@@ -1,18 +1,21 @@
 use futures::{future, StreamExt};
-use ros2_client::{Context, Node, NodeOptions};
-use rustdds::{
-  policy::{self, Deadline, Lifespan},
-  Duration, QosPolicies, QosPolicyBuilder,
-};
+use ros2_client::*;
+
 pub fn main() {
-  let mut node = create_node();
-  let topic_qos = create_qos();
+  let context = Context::new().unwrap();
+  let mut node = context
+    .new_node(
+      "rustdds_listener",
+      "/rustdds",
+      NodeOptions::new().enable_rosout(true),
+    )
+    .unwrap();
 
   let chatter_topic = node
     .create_topic(
       "/topic",
       String::from("std_msgs::msg::dds_::String_"),
-      &topic_qos,
+      &ros2_client::DEFAULT_SUBSCRIPTION_QOS,
     )
     .unwrap();
   let chatter_subscription = node
@@ -24,40 +27,12 @@ pub fn main() {
       Err(e) => eprintln!("Receive request error: {:?}", e),
     }
   });
+
+  rosout!(node, ros2::LogLevel::Info, "wow  very listening   such topics  much subscribe.");
+
   smol::block_on(async {
     subscription_stream
       .for_each(|_result| future::ready(()))
       .await
   });
-}
-
-fn create_qos() -> QosPolicies {
-  let service_qos: QosPolicies = {
-    QosPolicyBuilder::new()
-      .history(policy::History::KeepLast { depth: 10 })
-      .reliability(policy::Reliability::Reliable {
-        max_blocking_time: Duration::from_millis(100),
-      })
-      .durability(policy::Durability::Volatile)
-      .deadline(Deadline(Duration::INFINITE))
-      .lifespan(Lifespan {
-        duration: Duration::INFINITE,
-      })
-      .liveliness(policy::Liveliness::Automatic {
-        lease_duration: Duration::INFINITE,
-      })
-      .build()
-  };
-  service_qos
-}
-
-fn create_node() -> Node {
-  let context = Context::new().unwrap();
-  context
-    .new_node(
-      "rustdds_listener",
-      "/rustdds",
-      NodeOptions::new().enable_rosout(true),
-    )
-    .unwrap()
 }
