73a35e39ad778fb77f3ebd5d42d9c6a17309f07c
Juhana Helovuo <juhana.helovuo@atostek.com>
2024-04-15 13:19:58 +0300

    Move steady_time::Time implementation on top od std::time::Instant instead of raw libc call, because the latter is not portable.

--- src/steady_time.rs
+++ src/steady_time.rs
@@ -1,44 +1,55 @@
-// Steady time has a arbitrary origin, but is guaranteed to run
-// monotonically, regardless of clock corrections or timezones.
-//
-// Steady time should be used onyl when it is necessary, e.g. because of
-// interacting with hardware. Steady time cannot be simulated. Use ROS time
-// instead, whenever possible.
-//
+//! Steady time has an arbitrary origin, but is guaranteed to run
+//! monotonically (i.e. non-decreasing), regardless of clock corrections or
+//! timezones.  
+//!
+//! *Note* Steady time is not actually steady in the sense that the underlying
+//! clock would always run. Steady time clock might be paused e.g. because of
+//! operating system sleep modes, hypervisors, or other infrastructure below
+//! this library. This module is called `steady_time` only to better conform to
+//! ROS 2 naming.
+//!
+//! The current implementation is based on `std::time::Instant`. Because of
+//! this, there is no directo conversion to/from nanoseconds, or other time
+//! types.
+//!
+//! Steady time should be used only when it is necessary, e.g. because of
+//! interacting with hardware. Steady time cannot be simulated. Use ROS time
+//! instead, whenever possible.
 
 use std::{
-  convert::TryFrom,
+  cmp::Ordering,
   fmt,
   ops::{Add, Sub},
-  time::Duration,
+  time::{Duration, Instant},
 };
 
-use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
 
-// Monotonic time in nanoseconds
-//H
-// To get offset to UTC time, use now_with_utc() note that this will change over
-// time, latest at the next leap second.
-#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Serialize, Deserialize)]
+use crate::ROSTime;
+
+/// Monotonic time in nanoseconds
+///
+/// To get offset to UTC time, use now_with_utc() note that the offset will
+/// change over time, latest at the next leap second.
+#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug)]
 pub struct Time {
-  nano_counter: i64, // free-running nanosecond counter
+  instant: Instant,
 }
 
 impl Time {
   pub fn now() -> Time {
-    let mut ts = libc::timespec {
-      tv_sec: 0,
-      tv_nsec: 0,
-    };
-    unsafe {
-      assert_eq!(0, libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut ts));
-    }
-    Time {
-      nano_counter: ts.tv_sec * (10i64.pow(9)) + ts.tv_nsec,
+    Self {
+      instant: Instant::now(),
     }
   }
 
+  /// returns the current time in two formats: Time and ROSTime
+  pub fn now_with_ros_time() -> (Time, ROSTime) {
+    let (st, ct) = Self::now_with_utc();
+    (st, ct.into())
+  }
+
+  #[doc(hidden)]
   pub fn now_with_utc() -> (Time, DateTime<Utc>) {
     let m0 = Self::now();
     let utc = Utc::now();
@@ -50,23 +61,7 @@ impl Time {
     // We add half of the diff to compensate for the difference in call times.
     (m0 + TimeDiff::from_nanos(diff.as_nanos() / 2), utc)
   }
-
-  pub fn as_i64(self) -> i64 {
-    self.nano_counter
-  }
-
-  pub fn from_i64(nanos: i64) -> Time {
-    Time {
-      nano_counter: nanos,
-    }
-  }
-
-  // Time cannot implement the Zero trait, because Time does not implement Add
-  // (with itself). So we just implement a zero() function.
-  pub fn zero() -> Time {
-    Time { nano_counter: 0 }
-  }
-}
+} // impl Time
 
 impl fmt::Display for Time {
   fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
@@ -79,80 +74,142 @@ impl Sub for Time {
   type Output = TimeDiff;
 
   fn sub(self, other: Time) -> TimeDiff {
-    TimeDiff::from_nanos(self.nano_counter - other.nano_counter)
+    self
+      .instant
+      .checked_duration_since(other.instant)
+      // This fails if other > self. Then we try the other way around
+      .map(|duration| TimeDiff {
+        duration,
+        is_negative: false,
+      })
+      .unwrap_or_else(|| TimeDiff {
+        duration: other.instant.saturating_duration_since(self.instant),
+        is_negative: true,
+      })
   }
 }
 
+/// Note: This may panic on over/underflows
 impl Sub<TimeDiff> for Time {
   type Output = Time;
 
-  fn sub(self, other: TimeDiff) -> Time {
-    Time {
-      nano_counter: self.nano_counter - other.as_nanos(),
+  fn sub(self, diff: TimeDiff) -> Time {
+    if diff.is_negative {
+      Time {
+        instant: self.instant - diff.duration,
+      }
+    } else {
+      Time {
+        instant: self.instant + diff.duration,
+      }
     }
   }
 }
 
+/// Note: This may panic on over/underflows
 impl Add<TimeDiff> for Time {
   type Output = Time;
 
-  fn add(self, other: TimeDiff) -> Time {
-    Time {
-      nano_counter: self.nano_counter + other.as_nanos(),
+  fn add(self, diff: TimeDiff) -> Time {
+    if diff.is_negative {
+      Time {
+        instant: self.instant + diff.duration,
+      }
+    } else {
+      Time {
+        instant: self.instant - diff.duration,
+      }
     }
   }
 }
 
-// time difference as nanoseconds, can be negative, unlike std::time::Duration
-#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Serialize, Deserialize)]
+/// Time difference can be negative, unlike std::time::Duration
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct TimeDiff {
-  nano_diff: i64,
+  duration: Duration,
+  is_negative: bool, // must be false if duration == Duration::ZERO
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct NegativetimeDiffError {}
+
 impl TimeDiff {
-  pub fn as_i64(self) -> i64 {
-    self.nano_diff
-  }
-
-  pub fn from_i64(nanos: i64) -> TimeDiff {
-    TimeDiff { nano_diff: nanos }
-  }
-
   pub const fn from_nanos(nanos: i64) -> TimeDiff {
-    TimeDiff { nano_diff: nanos }
+    if nanos >= 0 {
+      TimeDiff {
+        duration: Duration::from_nanos(nanos as u64),
+        is_negative: false,
+      }
+    } else {
+      TimeDiff {
+        duration: Duration::from_nanos(-nanos as u64),
+        is_negative: true,
+      }
+    }
   }
 
   pub const fn from_millis(millis: i64) -> TimeDiff {
-    TimeDiff {
-      nano_diff: millis * 1_000_000,
-    }
+    Self::from_nanos(millis * 1_000_000)
   }
 
   pub const fn from_secs(secs: i64) -> TimeDiff {
-    TimeDiff {
-      nano_diff: secs * 1_000_000_000,
-    }
+    Self::from_nanos(secs * 1_000_000_000)
   }
 
   pub const fn as_nanos(self) -> i64 {
-    self.nano_diff
+    let n = self.duration.as_nanos();
+    let n = if n > (i64::MAX as u128) {
+      i64::MAX
+    } else {
+      n as i64
+    };
+    if self.is_negative {
+      -n
+    } else {
+      n
+    }
   }
 
   pub const fn as_millis(self) -> i64 {
-    self.nano_diff / 1_000_000
+    self.as_nanos() / 1_000_000
   }
 
   #[allow(dead_code)]
   pub const fn as_seconds(self) -> i64 {
-    self.nano_diff / 1_000_000_000
+    self.as_nanos() / 1_000_000_000
   }
 
-  pub fn as_duration(self) -> Result<Duration, <u64 as TryFrom<i64>>::Error> {
-    u64::try_from(self.nano_diff).map(Duration::from_nanos)
+  pub fn as_duration(self) -> Result<Duration, NegativetimeDiffError> {
+    if self.is_negative {
+      Err(NegativetimeDiffError {})
+    } else {
+      Ok(self.duration)
+    }
   }
 
   pub fn as_saturating_duration(self) -> Duration {
-    self.as_duration().unwrap_or(Duration::from_secs(0))
+    if self.is_negative {
+      Duration::ZERO
+    } else {
+      self.duration
+    }
+  }
+}
+
+impl Ord for TimeDiff {
+  fn cmp(&self, other: &Self) -> Ordering {
+    match (self.is_negative, other.is_negative) {
+      (false, false) => self.duration.cmp(&other.duration),
+      (true, true) => self.duration.cmp(&other.duration).reverse(),
+      (false, true) => Ordering::Greater,
+      (true, false) => Ordering::Less,
+    }
+  }
+}
+
+impl PartialOrd for TimeDiff {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
   }
 }
 
@@ -166,17 +223,13 @@ impl fmt::Display for TimeDiff {
 impl Add for TimeDiff {
   type Output = TimeDiff;
   fn add(self, other: TimeDiff) -> TimeDiff {
-    TimeDiff {
-      nano_diff: self.nano_diff + other.nano_diff,
-    }
+    Self::from_nanos(self.as_nanos() + other.as_nanos())
   }
 }
 
 impl Sub for TimeDiff {
   type Output = TimeDiff;
   fn sub(self, other: TimeDiff) -> TimeDiff {
-    TimeDiff {
-      nano_diff: self.nano_diff - other.nano_diff,
-    }
+    Self::from_nanos(self.as_nanos() - other.as_nanos())
   }
 }
